857632425b2b4c9b360b580534c705133a185117
kmh-no3 <lmbd.cs2nc.manga@gmail.com>
2026-01-02 15:24:34 +0900

    Initial Commit

---
+++ lorepocket-ios/lorepocket_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lorepocket_iosApp.swift
+//  lorepocket-ios
+//
+//  Created by 細田健司 on 2026/01/02.
+//
+
+import SwiftUI
+
+@main
+struct lorepocket_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
